373aac4dcd2cc6364eba4be46fa935893e3a5961
Malind Kowalski <malind.nikulski@gmail.com>
2017-10-17 17:26:47 -0600

    finished fetch code along

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'c481a65484839ae5de3cd2f974b1670de6fb2ec3'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
